fa5361720bc3a5bfc44e896158f188d05036dfb5
Joel  Godoy <j.godoy.moraes@gmail.com>
2023-01-31 09:55:48 -0300

    Comparator implemented as anonymous class in class Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
@@ -17,7 +18,16 @@ public class Program {
 		list.add(new Product("Phone",1100.00));
 		list.add(new Product("Watch", 250.00));
 		
-		list.sort(new MyComparator());
+		//Comparator as anonymous class
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+			
+		};
+		
+		list.sort(comp);
 
 		//Print products ordered by name
 		for(Product p:list) {
