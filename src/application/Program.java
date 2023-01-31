3a23db7c592e45fdfb79f6a0f7fd5c84dd24f570
Joel  Godoy <j.godoy.moraes@gmail.com>
2023-01-31 10:00:04 -0300

    Comparator implemented through lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,12 +18,10 @@ public class Program {
 		list.add(new Product("Phone",1100.00));
 		list.add(new Product("Watch", 250.00));
 		
-		//Comparator as anonymous class
-		Comparator<Product> comp = new Comparator<Product>() {
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+		//Comparator implemented through lambda expression -- much less verbose
+		//This is an anonymous function (or arrow function)
+		Comparator<Product> comp = (p1,p2)-> { //syntax: (parameters) ->(arrow) { implementation};
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());	
 			
 		};
 		
